e2e46e4dfc2056320bda980c4ddf02e707323fd6
TheGiraffeInTheBathtub <franciszek.ryt.9227@student.uu.se>
2025-02-16 18:16:41 +0100

    sky sprite change code

---
+++ Assets/SkyChange.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SkyChange : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(DayCycle.day)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+        }
+
+        if(!DayCycle.day)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+        }
+    }
+}
